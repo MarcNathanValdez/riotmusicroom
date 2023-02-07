193a52561551f04dfde73b6f28879ddaf70f5adf
MarcNathanValdez <81819491+MarcNathanValdez@users.noreply.github.com>
2023-02-07 19:34:48 +0800

    First Commit

---
+++ explore.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+ 
+  <title>Explore | Riot Music Room Studio</title>
+
+  <!-- Favicons -->
+  <link href="img/main_img/logo.png" rel="icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Kelly - v4.9.1
+  * Template URL: https://bootstrapmade.com/kelly-free-bootstrap-cv-resume-html-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+  <!-- ===== BG =====-->
+<div style="background-image: url('img/about_img/about_bg_3rd.gif'); 
+    background-repeat: no-repeat;background-size: cover;">
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top" style="background-image: url('img/about_img/about_bg_3rd.gif'); 
+    background-repeat: no-repeat;background-size: cover;">
+    <div class="container-fluid d-flex justify-content-between align-items-center">
+
+      <!-- ======= logo ======= -->
+      <a href="index.html"><img src="img/main_img/logo.png" alt="" class="img-fluid" width="100" height="70"></a>
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a href="index.php">HOME</a></li>
+          <li><a href="about.php">ABOUT US</a></li>
+          <li><a href="book.php">BOOK NOW</a></li>
+          <li><a class="active" href="explore.php">EXPLORE</a></li>
+          <li><a href="contact.php">CONTACT</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <div class="header-social-links">
+        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+        <a href="https://www.facebook.com/MNVBandRehearsal" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+      </div>
+
+    </div>
+
+  </header><!-- End Header -->
+
+
+  <main id="main">
+
+    <!-- ======= Portfolio Section ======= -->
+    <section id="portfolio" class="portfolio">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title" style="color: rgb(210, 210, 210);">
+          <h2>EXPLORE THE STUDIO</h2>
+          <p>You can play and enjoy different kinds of instruments.</p>
+        </div>
+
+        <div class="row" data-aos="fade-up" data-aos-delay="100" >
+          <div class="col-lg-12 d-flex justify-content-center">
+            <ul id="portfolio-flters">
+              <li data-filter="*" class="filter-active">All</li>
+              <li data-filter=".filter-app">WHOLE VIEW</li>
+              <li data-filter=".filter-card">INSTRUMENTS</li>
+              <li data-filter=".filter-web">GADGET</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="img/portfolio/exploreimg1.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Riot Music Studio</h4>
+                <p><i>click the + to view</i></p>
+                <div class="portfolio-links">
+                  <a href="img/portfolio/exploreimg1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Riot Music Studio"><i class="bx bx-plus"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <div class="portfolio-wrap">
+              <img src="img/portfolio/portfolio-2.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Guitar Gadget</h4>
+                <p><i>click the + to view</i></p>
+                <div class="portfolio-links">
+                  <a href="img/portfolio/portfolio-2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Guitar Gadget"><i class="bx bx-plus"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="img/portfolio/exploreimg2.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Riot Music Studio</h4>
+                <p><i>click the + to view</i></p>
+                <div class="portfolio-links">
+                  <a href="img/portfolio/exploreimg2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Riot Music Studio"><i class="bx bx-plus"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <div class="portfolio-wrap">
+              <img src="img/hero-bg.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Drumset</h4>
+                <p><i>click the + to view</i></p>
+                <div class="portfolio-links">
+                  <a href="img/hero-bg.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 2"><i class="bx bx-plus"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <div class="portfolio-wrap">
+              <img src="img/portfolio/portfolio-5.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Keyboard Sustain Pedal</h4>
+                <p><i>click the + to view</i></p>
+                <div class="portfolio-links">
+                  <a href="img/portfolio/portfolio-5.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Keyboard Sustain Pedal"><i class="bx bx-plus"></i></a>
+                  <a href="portfolio-details.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="img/portfolio/exploreimg3.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Riot Music Studio</h4>
+                <p><i>click the + to view</i></p>
+                <div class="portfolio-links">
+                  <a href="img/portfolio/exploreimg3.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Riot Music Studio"><i class="bx bx-plus"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <div class="portfolio-wrap">
+              <img src="img/portfolio/exploreimg5.png" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Base Guitar</h4>
+                <p><i>click the + to view</i></p>
+                <div class="portfolio-links">
+                  <a href="img/portfolio/exploreimg5.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Base Guitar"><i class="bx bx-plus"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <div class="portfolio-wrap">
+              <img src="img/portfolio/exploreimg4.png" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Electric Guitar</h4>
+                <p><i>click the + to view</i></p>
+                <div class="portfolio-links">
+                  <a href="img/portfolio/exploreimg4.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Electric Guitar"><i class="bx bx-plus"></i></a>
+                  <a href="portfolio-details.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <div class="portfolio-wrap">
+              <img src="img/portfolio/portfolio-9.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Guitar Gadget Effect</h4>
+                <p><i>click the + to view</i></p>
+                <div class="portfolio-links">
+                  <a href="img/portfolio/portfolio-9.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Guitar Gadget Effect"><i class="bx bx-plus"></i></a>
+                  <a href="portfolio-details.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <div class="portfolio-wrap">
+              <img src="img/portfolio/exploreimg6.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Keyboard Piano</h4>
+                <p><i>click the + to view</i></p>
+                <div class="portfolio-links">
+                  <a href="img/portfolio/exploreimg6.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Keyboard Piano"><i class="bx bx-plus"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <div class="portfolio-wrap">
+              <img src="img/portfolio/exploreimg7.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Electric Guitar</h4>
+                <p><i>click the + to view</i></p>
+                <div class="portfolio-links">
+                  <a href="img/portfolio/exploreimg7.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Electric Guitar"><i class="bx bx-plus"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <div class="portfolio-wrap">
+              <img src="img/portfolio/exploreimg8.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Microphone</h4>
+                <p><i>click the + to view</i></p>
+                <div class="portfolio-links">
+                  <a href="img/portfolio/exploreimg8.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Microphone"><i class="bx bx-plus"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Portfolio Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <br><br><br><br><br><br><br><br><br><br><br><br>
+  <footer id="footer">
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Kelly</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/kelly-free-bootstrap-cv-resume-html-template/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- End  Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
